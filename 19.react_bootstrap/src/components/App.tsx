bc9e6315846a38b313cfc6130806ddd5cffe0d27
zakir-ecl <zakir-ecl@experts.com.bd>
2020-11-05 23:54:19 +0600

    React-Bootstrap

---
+++ 19.react_bootstrap/src/components/App.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card, Col, Container, Row, Navbar,Nav,Form, FormControl, Carousel } from 'react-bootstrap';
+
+export const App=()=>{
+    return(
+    <>
+      <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+        <Container>
+        <Row>
+          <Col xs>
+          <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://picsum.photos/200" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+          </Col>
+          <Col xs>
+          <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://picsum.photos/200" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+          </Col>
+          <Col xs>
+          <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://picsum.photos/200" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+          </Col>
+        </Row>
+        <br/>
+        <br/>
+        <br/>
+        
+        <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 "
+      src="https://picsum.photos/200"
+      alt="First slide"
+      height="300px"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://picsum.photos/200"
+      alt="Third slide"
+      height="300px"
+      
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://picsum.photos/200"
+      alt="Third slide"
+      height="300px"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+<br/><br/> <br/>
+
+        
+        <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+      </Container>
+</>
+    );
+}
