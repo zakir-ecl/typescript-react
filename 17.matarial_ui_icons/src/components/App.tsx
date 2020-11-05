e3123b726ce4080c55817e78f345c48fb037ec91
zakir-ecl <zakir-ecl@experts.com.bd>
2020-11-05 20:29:41 +0600

    I use material icon using NPM

---
+++ 17.matarial_ui_icons/src/components/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import HomeIcon from '@material-ui/icons/Home';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import BluetoothIcon from '@material-ui/icons/Bluetooth';
+
+export const App=()=>{
+    return(
+        <>
+        <h1>Add Icon <AddIcon/></h1>
+        <h1>Home Icon <HomeIcon/></h1>
+        <h1>Delete Icon <DeleteIcon/></h1>
+        <h1>Smile Icon <EmojiEmotionsIcon/></h1>
+        <h1>Bluetooth Icon <BluetoothIcon/></h1>
+        </>
+    
+    );
+
+}
